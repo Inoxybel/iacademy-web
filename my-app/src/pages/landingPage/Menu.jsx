7f24d8aacab70d15d3a7c141b227545d4e264bf4
Assis <lucas.assis.external@sonova.com>
2023-10-19 13:00:24 -0300

    att

--- my-app/src/pages/landingPage/Menu.jsx
+++ my-app/src/pages/landingPage/Menu.jsx
@@ -1,6 +1,6 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import MenuDropAction from "./menuDropAction";
+import MenuDropAction from "./MenuDropAction";
 import { useState } from 'react';
 
 const MenuLandingPage = ({ breakpoints, breakpoint }) => {
