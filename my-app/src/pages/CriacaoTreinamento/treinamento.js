c35dd29f354a15672299abe4e5a5bcb3f96a3fab
IgorEverton <evertonalves369@gmail.com>
2023-10-20 00:01:04 -0300

    função get

--- my-app/src/pages/CriacaoTreinamento/treinamento.js
+++ my-app/src/pages/CriacaoTreinamento/treinamento.js
@@ -27,17 +27,15 @@ import Menu from "../Menu";
 import axios from 'axios';
 import { AddIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
-import { pegarConfiguracao, atualizarConfiguracao, novaConfiguracao} from '../../services/Fetchers/FetchersApp';
-import {getConfiguration} from './banco.js';
-import { useQuery } from 'react-query';
+
+const apiUrl = 'https://my-json-server.typicode.com/IgorEverton/fakeApiTest/configuracoes';
 
 function Treinamento() {
-
-  const [opcoes, setOpcoes] = useState([]);
-  const [configuracoes, setConfiguracoes] = useState([]);
+  const [data, setData] = useState([]);
   const navigate = useNavigate();
   const [activeIndex, setActiveIndex] = useState(0);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [nomeConfiguracao, setNomeConfiguracao] = useState('');
   const [inputValue1, setInputValue1] = useState('');
   const [inputValue2, setInputValue2] = useState('');
   const [inputValue3, setInputValue3] = useState('');
@@ -50,16 +48,75 @@ function Treinamento() {
   const [inputValue10, setInputValue10] = useState('');
   const [inputValue11, setInputValue11] = useState('');
   const [inputValue12, setInputValue12] = useState('');  
+  const [idConfiguracao, setIdConfiguracao] = useState('');  
+ 
 
   useEffect(() => {
-      // Converte as configurações para o formato desejado para as opções do Select
-      const configuracoes1 = getConfiguration.map((config) => (
-        <option key={config.id} value={config.valor} onClick={()=>alert("Clicando")}>
-          {config.name}
-        </option>
-      ));
-      setOpcoes(configuracoes1); // Atualiza o estado com as opções formatadas
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        setData(response.data);
+      } catch (error) {
+        console.error('Erro ao obter configurações', error);
+      }
+    };
+    fetchData();
   }, []);
+
+  const onSelect = (configSelecionada) => {
+    if (configSelecionada) {
+      const nmConfig = configSelecionada.name;
+      setNomeConfiguracao(nmConfig);
+  
+      // Chama a função getDataById apenas se configSelecionada for um objeto válido
+      if (configSelecionada.id) {
+        const idDaConfig = configSelecionada.id;
+        setIdConfiguracao(idDaConfig);
+        getDataById(idDaConfig);  // Certifique-se de que a função está correta
+        console.log('ID da Configuração Selecionada:', idDaConfig);
+      } else {
+        console.error('Objeto de configuração inválido:', configSelecionada);
+        // Lide com a situação quando o objeto é inválido
+        // Por exemplo, mostrar uma mensagem de erro ou limpar os estados
+      }
+    }
+  };
+
+  const getDataById = async (id) => {
+    try {
+      let response;
+  
+      if (id) {
+        // Se um ID foi fornecido, busca a configuração correspondente
+        response = await axios.get(`${apiUrl}/${id}`);
+        const configuracao = response.data;  // Corrija aqui
+        popularEstadosPorId(configuracao);
+      } else {
+        // Caso contrário, busca todas as configurações
+        response = await axios.get(apiUrl);
+      }
+  
+      console.log('Dados da Configuração obtidos com sucesso:', response.data);  // Mude aqui se necessário
+    } catch (error) {
+      console.error('Erro ao buscar dados da configuração:', error);
+    }
+  };
+  
+
+  const popularEstadosPorId = (configuracao) => {
+    setInputValue1(configuracao.summary.initialInput);
+    setInputValue2(configuracao.summary.finalInput);
+    setInputValue3(configuracao.firstContent.initialInput);
+    setInputValue4(configuracao.firstContent.finalInput);
+    setInputValue5(configuracao.newContent.initialInput);
+    setInputValue6(configuracao.newContent.finalInput);
+    setInputValue7(configuracao.exercise.initialInput);
+    setInputValue8(configuracao.exercise.finalInput);
+    setInputValue9(configuracao.correction.initialInput);
+    setInputValue10(configuracao.correction.finalInput);
+    setInputValue11(configuracao.pendency.initialInput);
+    setInputValue12(configuracao.pendency.finalInput);
+  };
 
   const handleInputChange = (e, identifier) => {
     const inputValue = e.target.value;
@@ -145,19 +202,32 @@ function Treinamento() {
     };
   }
 
-  const enviarParaAPI =()=> {
+  const enviarParaAPI = async () => {
     const obj = criarObjeto();
-    try {
-      const resposta = getConfiguration(obj);
-      if (resposta.ok) {
-        console.log('Configuração enviada com sucesso!');
-      } else {
-        console.error('Erro ao enviar configuração:', resposta.statusText);
+    if (idConfiguracao) {
+      try {
+        const resposta = await axios.put(`https://my-json-server.typicode.com/IgorEverton/fakeApiTest/configuracoes/${idConfiguracao}`, obj);
+        if (resposta.status === 200) {
+          console.log('Configuração atualizada com sucesso!');
+        } else {
+          console.error('Erro ao atualizar configuração:', resposta.statusText);
+        }
+      } catch (erro) {
+        console.error('Erro na requisição:', erro.message);
       }
-    } catch (erro) {
-      console.error('Erro na requisição:', erro);
+    } else {
+      try {
+        const resposta = await axios.post('https://my-json-server.typicode.com/IgorEverton/fakeApiTest/configuracoes', obj);
+        if (resposta.status === 201) {
+          console.log('Configuração enviada com sucesso!');
+        } else {
+          console.error('Erro ao enviar configuração:', resposta.statusText);
+        }
+      } catch (erro) {
+        console.error('Erro na requisição:', erro.message);
+      }
     }
-  };
+  };  
 
   const limpaCampos=()=>{
     setInputValue1("");
@@ -180,8 +250,12 @@ function Treinamento() {
       <Heading>Criar novo Treinamento</Heading>
       <VStack alignItems={"center"} bg={"#282B38"} p="4" gap="2rem" borderRadius={7}>
         <Heading size={12}>Configuração</Heading>      
-        <Select placeholder='Select option' spacing={3} icon={<ChevronDownIcon />} bg="white" color="black">
-          {opcoes.length > 0 ? opcoes  : <option  value="">Carregando...</option>}
+        <Select placeholder='Select option' spacing={3} icon={<ChevronDownIcon />} bg="white" color="black" onChange={(e) => onSelect(data.find(config => config.id === e.target.value))}>
+          {data.map((config) => (
+            <option key={config?.id} value={config?.id}>
+              {config?.name}
+            </option>
+          ))}
         </Select>
         <Flex direction="row" p={0} w="100%" justifyContent="space-between" align={"center"}>
           <IconButton aria-label='Add to friends' icon={<AddIcon />} onClick={onOpen}/>
@@ -197,7 +271,7 @@ function Treinamento() {
         >
         <ModalOverlay />
         <ModalContent bg="#282B38" color="white">
-          <ModalHeader>Nome_Configuração_Atual</ModalHeader>
+          <ModalHeader>{nomeConfiguracao}</ModalHeader>
           <ModalCloseButton onClick={()=>{
             setActiveIndex(0);
             limpaCampos();
@@ -368,7 +442,10 @@ function Treinamento() {
                 />
                 <Flex justifyContent={"space-evenly"}>
                 <Button onClick={handlePreviousTab}>Anterior</Button>
-                <Button bg="#0880A2" color="white" onClick={enviarParaAPI}>Salvar Alterações</Button>
+                <Button bg="#0880A2" color="white" onClick={()=>{
+                  enviarParaAPI();
+                  onClose();
+                  }}>Salvar Alterações</Button>
                 </Flex>
               </TabPanel>
             </TabPanels>
