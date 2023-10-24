8fb3f1d5bd33384a7b4d8001d4a9480e49d90a87
IgorEverton <evertonalves369@gmail.com>
2023-10-24 14:25:16 -0300

    btão para setar imagem

--- my-app/src/pages/CriacaoTreinamento/nextTreinamento.js
+++ my-app/src/pages/CriacaoTreinamento/nextTreinamento.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 import {
   Box,
   Heading,
@@ -11,103 +11,160 @@ import {
   Input,
   Grid,
   GridItem,
-} from "@chakra-ui/react";
-import styles from "../styles.js";
-import Menu from "../Menu";
-import axios from 'axios';
-import { AddIcon, ChevronDownIcon, ArrowBackIcon } from "@chakra-ui/icons";
-import { pegarConfiguracao, atualizarConfiguracao, novaConfiguracao} from '../../services/Fetchers/FetchersApp';
-import { useNavigate } from "react-router-dom";
-
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+} from '@chakra-ui/react';
+import { ChevronDownIcon, ArrowBackIcon } from '@chakra-ui/icons';
+import Menu from '../Menu';
+import { useNavigate } from 'react-router-dom';
 
 function Treinamento() {
-  const [tema, setTema] = useState("");
-  const [categoria, setCategoria] = useState("");
-  const [subcategoria, setSubcategoria] = useState("");
-  const [identificacao, setIdentificacao] = useState("");
+  const [tema, setTema] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [subcategoria, setSubcategoria] = useState('');
+  const [identificacao, setIdentificacao] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [inputUrl, setInputUrl] = useState('');
+  const [isModalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
 
-  function criarObj(){
-    return(
-      tema, categoria, subcategoria, identificacao
-      );
-  }
+  const criarObj = () => {
+    return {
+      tema,
+      categoria,
+      subcategoria,
+      identificacao,
+    };
+  };
 
   const enviarParaAPI = async () => {
-    // Crie o objeto de configuração
     const obj = criarObj();
-  
+
     try {
-      // Chame a função novaConfiguracao passando o objeto de configuração
-      const resposta = await atualizarConfiguracao(obj);
-  
-      // Verifique a resposta ou lide com erros, se necessário
-      if (resposta.ok) {
-        console.log('Configuração atualizada com sucesso!');
-      } else {
-        console.error('Erro ao atualizar configuração:', resposta.statusText);
-      }
+      // Simulando uma requisição para atualizar a configuração
+      console.log('Enviando para a API:', obj);
     } catch (erro) {
       console.error('Erro na requisição:', erro);
     }
   };
 
+  const handleInputChange = (e) => {
+    setInputUrl(e.target.value);
+  };
+
+  const setNewImage = () => {
+    setImageUrl(inputUrl);
+    setModalOpen(false); // Fecha o modal após definir a imagem
+  };
+
   return (
-    <Flex maxW='vw' mx="auto"  >
-      <Menu/>
+    <Flex maxW="vw" mx="auto">
+      <Menu />
       <Flex>
-        <ArrowBackIcon  w={6} h={6} m={2} onClick={()=> navigate("/Treinamento")}_hover={{cursor:"pointer"}} />
+        <ArrowBackIcon
+          w={6}
+          h={6}
+          m={2}
+          onClick={() => navigate('/Treinamento')}
+          _hover={{ cursor: 'pointer' }}
+        />
       </Flex>
-      <Flex direction={"column"} gap="2rem" p={5}>
-        <Heading  align="center">Criar novo Treinamento</Heading>
-        <VStack alignItems={"center"} bg={"#282B38"} p="3" borderRadius={7}>
-          <Heading size={12}>Informações do Treinamento</Heading>      
-          <Flex align={"center"} justifyContent={"space-between"} gap="1.5rem">
-            <Image src='gibbresh.png' fallbackSrc='https://via.placeholder.com/90' borderRadius={7} />
-            <Input 
-              value={tema}
-              onChange={setTema}
-              placeholder='Tema' 
-              mx="auto"/>
-            <Input 
+      <Flex direction={'column'} gap="2rem" p={5}>
+        <Heading align="center">Criar novo Treinamento</Heading>
+        <VStack alignItems={'center'} bg={'#282B38'} p="3" borderRadius={7}>
+          <Heading size={12}>Informações do Treinamento</Heading>
+          <Flex align={'center'} justifyContent={'space-between'} gap="1rem">
+            <Box>
+              <Image
+                src={imageUrl || 'https://via.placeholder.com/90'}
+                borderRadius={7}
+                alt="Descrição da imagem"
+                h={120}
+                w={140}
+              />
+              <Button onClick={() => setModalOpen(true)} mt={5}>
+                Definir Imagem
+              </Button>
+            </Box>
+            <Input value={tema} onChange={(e) => setTema(e.target.value)} placeholder="Tema" mx="auto" />
+            <Input
               value={categoria}
-              onChange={setCategoria}
-              placeholder='Categoria' 
-              mx="auto" />
-            <Input 
+              onChange={(e) => setCategoria(e.target.value)}
+              placeholder="Categoria"
+              mx="auto"
+            />
+            <Input
               value={subcategoria}
-              onChange={setSubcategoria}
-              placeholder='Subcategoria' 
-              mx="auto"/>
-            <Input 
+              onChange={(e) => setSubcategoria(e.target.value)}
+              placeholder="Subcategoria"
+              mx="auto"
+            />
+            <Input
               value={identificacao}
-              onChange={setIdentificacao}
-              placeholder='Identificação' 
-              mx="auto"/>
+              onChange={(e) => setIdentificacao(e.target.value)}
+              placeholder="Identificação"
+              mx="auto"
+            />
           </Flex>
-            <Flex alignSelf={"flex-end"}>
-              <Button bg="#3C485A" color={"white"} onClick={enviarParaAPI}>Criar Base</Button>
-            </Flex>
+          <Flex alignSelf={'flex-end'}>
+            <Button bg="#3C485A" color={'white'} onClick={enviarParaAPI}>
+              Criar Base
+            </Button>
+          </Flex>
         </VStack>
-        <Grid templateColumns="repeat(2, 1fr)" gap={4} >
-          <GridItem colSpan={1} bg={"#282B38"} h="30rem" w="33rem" p={5}>
-            <Heading as="h3" size="sm" mb="0.2rem">Subtópico:</Heading>
-            <Flex  mb={"1rem"}>
-              <Select placeholder='Selecione Subtópico'spacing={3} icon={<ChevronDownIcon />} bg="white" color="black">
-                <option value='option1'>Option 1</option>
-                <option value='option2'>Option 2</option>
-                <option value='option3'>Option 3</option>
+        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+          <GridItem colSpan={1} bg={'#282B38'} h="30rem" w="33rem" p={5}>
+            <Heading as="h3" size="sm" mb="0.2rem">
+              Subtópico:
+            </Heading>
+            <Flex mb={'1rem'}>
+              <Select
+                placeholder="Selecione Subtópico"
+                spacing={3}
+                icon={<ChevronDownIcon />}
+                bg="white"
+                color="black"
+              >
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
               </Select>
-              <Button ml="5"bg="#3C485A" color={"white"}>Criar</Button>
+              <Button ml="5" bg="#3C485A" color={'white'}>
+                Criar
+              </Button>
             </Flex>
-            <Textarea placeholder='Here is a sample placeholder' size="lg"  bg="white" minH="340px" color="black"/>
+            <Textarea placeholder="Here is a sample placeholder" size="lg" bg="white" minH="340px" color="black" />
           </GridItem>
-          <GridItem colSpan={1} bg={"#282B38"} p={5}>
-          <Heading as="h3" size="sm" mb="0.5rem">Exercícios:</Heading>
-            <Textarea placeholder='Here is a sample placeholder' size="lg"  bg="white" minH="390px" color="black"/>
+          <GridItem colSpan={1} bg={'#282B38'} p={5}>
+            <Heading as="h3" size="sm" mb="0.5rem">
+              Exercícios:
+            </Heading>
+            <Textarea placeholder="Here is a sample placeholder" size="lg" bg="white" minH="390px" color="black" />
           </GridItem>
         </Grid>
       </Flex>
+
+      {/* Modal para inserir a URL da imagem */}
+      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Definir Imagem</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Input
+              placeholder="Insira a URL da imagem"
+              value={inputUrl}
+              onChange={handleInputChange}
+              mb={4}
+            />
+            <Button onClick={setNewImage}>Definir</Button>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
